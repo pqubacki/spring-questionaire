633579bcb9251a9cb9177f73c4fabb70196c2546
Piotr Kubacki <piotr.kubacki@solera.com>
2022-11-07 10:04:09 +0100

    Initial commit

---
+++ questionaire/src/test/java/com/solera/questionaire/QuestionaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solera.questionaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
